69d0e2527616cb61ed62f2bf4dd0c4c155cebf82
Bill Cheng <billcheng@Bills-MacBook-Pro.local>
2016-08-23 19:04:58 -0700

    added matching?

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,4 +2,9 @@ console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardThree = "king";
+var cardFour = "king";
+
+if (cardOne === cardTwo)
+{
+	alert("You found a match!");
+}
